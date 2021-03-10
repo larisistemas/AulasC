8764753e8ed8c35f481702ba8cd02eda9df1dfcd
Larissa Cardoso <79551053+larisistemas@users.noreply.github.com>
2021-03-09 22:20:18 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    int i=0, j=0;
+    printf("Digite o primeiro valor na tela:\n");
+    scanf("%i",&i);
+    printf("Digite o segundo valor na tela:\n");
+    scanf("%i",&j);
+    printf("Resultado: %i",i+j);
+    //system("pause");
+}
